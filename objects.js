1c66f2822d13db2545eb43559d89c6d42033ed29
candescent-sequence-1308 <amandaashmen@gmail.com>
2020-08-08 03:38:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,8 +2,10 @@ var playlist = {jb: 'intentions'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist.artistName= songTitle
+  return playlist
 }
 
 function removeFromPlaylist(playlist, name) {
   delete playlist.name
+  return playlist
 }
